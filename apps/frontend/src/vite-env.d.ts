4c69ff47fac6bf5dd3b024897a22780abbf4b9ba
GAURAV07C <gaurav07c@gmail.com>
2026-07-09 09:55:25 +0530

    fix: explicitly include type declarations in tsconfig

--- apps/frontend/src/vite-env.d.ts
+++ apps/frontend/src/vite-env.d.ts
@@ -1,53 +1,51 @@
-/// <reference types="vite/client" />
-
 declare module 'react-dom' {
   export * from 'react-dom';
 }
-
 declare module 'react-dom/client' {
   export * from 'react-dom/client';
 }
-
+declare module 'uuid' {
+  const uuid: unknown;
+  export default uuid;
+}
 declare module '*.png' {
   const src: string;
   export default src;
 }
-
 declare module '*.jpg' {
   const src: string;
   export default src;
 }
-
 declare module '*.jpeg' {
   const src: string;
   export default src;
 }
-
 declare module '*.svg' {
   const src: string;
   export default src;
 }
-
 declare module '*.wav' {
   const src: string;
   export default src;
 }
-
 declare module '*.mp3' {
   const src: string;
   export default src;
 }
-
 declare module '*.gif' {
   const src: string;
   export default src;
 }
-
+declare module '*.ico' {
+  const src: string;
+  export default src;
+}
 interface ImportMetaEnv {
   readonly VITE_APP_BACKEND_URL: string;
   readonly VITE_APP_WS_URL: string;
+  readonly DEV: boolean;
+  readonly MODE: string;
 }
-
 interface ImportMeta {
   readonly env: ImportMetaEnv;
 }
